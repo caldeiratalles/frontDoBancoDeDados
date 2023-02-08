0a7b03b49b4aafc1296769e4ef4388961992c9de
TM <64755991+caldeiratalles@users.noreply.github.com>
2023-02-08 01:41:53 -0300

    finalizado

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,12 +1,11 @@
 import './Header.css';
 
-
 function Header(handleListPessoa){
     
      return(
         <div className="container-header">
             <div className="logo">
-                <h3>Logo</h3>
+                <img> src="../../img/logo.png"</img>
             </div>
 	</div>
     );
